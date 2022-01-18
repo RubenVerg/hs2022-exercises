cca2d5d5438a04415bc477a393b0fa95d95bdce5
RubenVerg <rubenverg@outlook.com>
2022-01-18 13:29:41 +0100

    :sparkles: Lecture 2

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,6 +2,11 @@ module Utils
     ( count
     , count2
     , (!?)
+    , mapEither
+    , mapRight
+    , mapRight2
+    , lookupAll
+    , safeSum
     ) where
 
 {- | GHC 9 comes with @GHC.Utils.Misc.count@, this is GHC 8 tho
@@ -32,3 +37,32 @@ _ !? n | n < 0 = Nothing
 xs !? n = foldr (\x r k -> case k of
     0 -> Just x
     _ -> r (k - 1)) (const Nothing) xs n
+
+
+mapEither :: (a -> a') -> (b -> b') -> Either a b -> Either a' b'
+mapEither f _ (Left x) = Left $ f x
+mapEither _ g (Right y) = Right $ g y
+
+mapRight :: (b -> b') -> Either a b -> Either a b'
+mapRight = mapEither id
+
+mapRight2 :: (b -> b' -> b'') -> Either a b -> Either a b' -> Either a b''
+mapRight2 _ (Left x) _ = Left x
+mapRight2 _ _ (Left y) = Left y
+mapRight2 f (Right x) (Right y) = Right $ f x y
+
+lookupAll :: Eq a => a -> [(a, b)] -> [b]
+lookupAll needle list = map snd $ filter (\x -> fst x == needle) list
+
+-- As per https://hackage.haskell.org/package/safe-decimal-0.2.1.0/docs/src/Numeric.Decimal.BoundedArithmetic.html#plusBounded
+{- | Checks for over-/under-flow before summing
+-}
+safeSum :: (Ord a, Num a, Bounded a) => a -> a -> Maybe a
+safeSum a b
+    | sameSign && signA == 1 && a > maxBound - b = Nothing
+    | sameSign && signA == -1 && a > minBound - b = Nothing
+    | otherwise = Just $ a + b
+    where
+        signA = signum a
+        signB = signum b
+        sameSign = signA == signB
